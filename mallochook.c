cff81b908e52a6f9657dd8b64fdea4fef20cf9b3
SeongJae Park <sj38.park@gmail.com>
2018-07-23 17:07:25 +0900

    mallochook: Add log
    Signed-off-by: SeongJae Park <sj38.park@gmail.com>

--- mallochook.c
+++ mallochook.c
@@ -2,10 +2,13 @@
 
 void *malloc(size_t size)
 {
+	fprintf(stderr, "malloc(%zu) hooked\n", size);
+
 	return NULL;
 }
 
 void free(void *ptr)
 {
+	fprintf(stderr, "free(%p) hooked\n", ptr);
 	return;
 }
